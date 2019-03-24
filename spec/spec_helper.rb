431012fb3057f7c80d0e0e21caecb664a175adc3
Dustin Van Buskirk <dev@vanbuskirk.me>
2019-03-23 22:33:03 -0700

    updates for codefresh

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,14 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+require 'allure-rspec'
+
+RSpec.configure do |config|
+    config.include AllureRSpec::Adaptor # Export Allure Reports
+end
+
+AllureRSpec.configure do |c|
+    c.output_dir = "/codefresh/volume/allure-results"
+    c.clean_dir = false
+end
 
 at_exit { ChefSpec::Coverage.report! }
--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,10 +1,13 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 require 'chefspec/cacher'
+require 'allure-rspec'
 
 RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version = '14.04'
+
+  config.include AllureRSpec::Adaptor # Export Allure Reports
 
   config.color = true
   config.alias_example_group_to :describe_recipe, type: :recipe
@@ -25,6 +28,11 @@ RSpec.configure do |config|
     mocks.syntax = :expect
     mocks.verify_partial_doubles = true
   end
+end
+
+AllureRSpec.configure do |c|
+    c.output_dir = "/codefresh/volume/allure-results"
+    c.clean_dir = false
 end
 
 at_exit { ChefSpec::Coverage.report! }
